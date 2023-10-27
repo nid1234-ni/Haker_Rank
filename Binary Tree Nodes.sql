b46fb3905c05b70c40028c40f9de33d7bccdeb6d
Nidhi Raj Patel <59306056+nid1234-ni@users.noreply.github.com>
2023-10-27 16:20:46 +0530

    Advanced Select
    Advanced Select

---
+++ Binary Tree Nodes.sql
@@ -0,0 +1,11 @@
+--Problem : https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+---Binary Tree Nodes
+SELECT N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM BST
+ORDER BY N;
